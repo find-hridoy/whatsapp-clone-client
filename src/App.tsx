25225cb6b779e4cd8a67ec96ffe2c4eb2dbdfe79
hridoy546 <mdhridoymia546@gmail.com>
2020-12-01 17:19:09 +0600

    connect to server

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './app.css';
 import Chat from './components/Chat/Chat';
 import Sidebar from './components/Sidebar/Sidebar';
+import Pusher from 'pusher-js';
 
  function App () {
+   useEffect(()=>{
+    const pusher = new Pusher('2be980cecf9a2313cfd7', {
+      cluster: 'mt1'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data : string) => {
+      alert(JSON.stringify(data));
+    });
+   }, [])
   return (
     <div className="app">
       <div className="app-body">
